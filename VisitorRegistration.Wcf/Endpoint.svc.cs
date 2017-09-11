c6e89197510162e00cc8927b9dfe4bc7a353487d
Julian Hüpppauff <julian@hueppauff.com>
2017-09-11 18:47:44 +0200

    StyleCop CleanUp

--- VisitorRegistration.Wcf/Endpoint.svc.cs
+++ VisitorRegistration.Wcf/Endpoint.svc.cs
@@ -1,48 +1,76 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.ServiceModel;
-using System.Text;
-
+﻿//-----------------------------------------------------------------------
+// <copyright file="Endpoint.svc.cs" company="None">
+// Copyright 2017 Jhueppauff
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// </copyright>
+//-----------------------------------------------------------------------
 namespace VisitorRegistration.Wcf
 {
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    /// <summary>
+    /// Endpoint Class
+    /// </summary>
     public class Endpoint : IEndpoint
     {
-        public void RegisterVisitor(string FirstName, string LastName, string Email, string HostEmail, string IDNumber, string signature)
+        /// <summary>
+        /// Register a new Visitor
+        /// </summary>
+        /// <param name="firstName">First Name of the Visitor</param>
+        /// <param name="lastName">Last Name of the Visitor</param>
+        /// <param name="email">eMail of the Visitor</param>
+        /// <param name="hostEmail">eMail of the responsible Host</param>
+        /// <param name="idNumber">ID Number</param>
+        /// <param name="signature">Signature Bash64 String</param>
+        public void RegisterVisitor(string firstName, string lastName, string email, string hostEmail, string idNumber, string signature)
         {
-            var Visitor = new Visitor()
+            var visitor = new Visitor()
             {
                 DateOfVisit = DateTime.Now,
-                Email = Email,
-                Last_Name = LastName,
-                First_Name = FirstName,
-                IDNumber = IDNumber,
+                Email = email,
+                Last_Name = lastName,
+                First_Name = firstName,
+                IDNumber = idNumber,
                 Printed = false,
-                HostEmail = HostEmail,
+                HostEmail = hostEmail,
                 Signature = signature
-
             };
+
             using (var db = new VisitorRegistrationEntities())
             {
-                db.Visitors.Add(Visitor);
+                db.Visitors.Add(visitor);
                 db.SaveChanges();
             }
         }
 
-        public Visitor GetVisitor(int ID)
+        /// <summary>
+        /// Get Visitor by ID
+        /// </summary>
+        /// <param name="id">ID of the Visitor</param>
+        /// <returns><see cref="Visitor"/></returns>
+        public Visitor GetVisitor(int id)
         {
             using (var db = new VisitorRegistrationEntities())
             {
-                var visitor = from v in db.Visitors where v.ID == ID select v;
+                var visitor = from v in db.Visitors where v.ID == id select v;
 
-                Visitor VisitorEntry = visitor.FirstOrDefault();
-                return VisitorEntry;
+                Visitor visitorEntry = visitor.FirstOrDefault();
+                return visitorEntry;
             }
-
         }
 
+        /// <summary>
+        /// Get a List of the type<see cref="Visitor"/> from the daily visitors
+        /// </summary>
+        /// <returns>Returns a list of<see cref="Visitor"/></returns>
         public List<Visitor> GetCurrentVisitors()
         {
             using (var db = new VisitorRegistrationEntities())
@@ -50,30 +78,31 @@ namespace VisitorRegistration.Wcf
                 db.Configuration.ProxyCreationEnabled = false;
                 var visitor = from v in db.Visitors where v.DateOfVisit == DateTime.Today select v;
 
-                List<Visitor> VisitorEntrys = new List<Visitor>();
-                foreach (Visitor VisitorEntry in visitor)
+                List<Visitor> visitorEntrys = new List<Visitor>();
+                foreach (Visitor visitorEntry in visitor)
                 {
-                    VisitorEntry.Signature = null;
-                    VisitorEntrys.Add(VisitorEntry);
+                    visitorEntry.Signature = null;
+                    visitorEntrys.Add(visitorEntry);
                 }
 
-
-                return VisitorEntrys;
+                return visitorEntrys;
             }
         }
 
-        public Guid GenerateVisitorPass(int ID)
+        /// <summary>
+        /// Generates the Visitor Pass
+        /// </summary>
+        /// <param name="id">Visitor ID</param>
+        /// <returns>Returns the File ID as <see cref="Guid"/></returns>
+        public Guid GenerateVisitorPass(int id)
         {
-            Visitor visitor = GetVisitor(ID);
-            Guid FileID = Guid.NewGuid();
+            Visitor visitor = this.GetVisitor(id);
+            Guid fileID = Guid.NewGuid();
 
             var application = new Microsoft.Office.Interop.Word.Application();
-            
-
             var document = application.Documents.Add(Template: Properties.Settings.Default.TemplateFile);
 
             application.Visible = false;
-
 
             foreach (Microsoft.Office.Interop.Word.MailMergeField field in document.MailMerge.Fields)
             {
@@ -92,7 +121,7 @@ namespace VisitorRegistration.Wcf
                     field.Select();
                     application.Selection.TypeText(visitor.Email);
                 }
-                else if(field.Code.Text.Contains("DateOfVisit"))
+                else if (field.Code.Text.Contains("DateOfVisit"))
                 {
                     field.Select();
                     application.Selection.TypeText(visitor.DateOfVisit.ToString());
@@ -103,48 +132,50 @@ namespace VisitorRegistration.Wcf
                     application.Selection.TypeText(visitor.HostEmail);
                 }
             }
+
             document.Fields.Update();
 
-            document.SaveAs2(FileName: Path.Combine(Properties.Settings.Default.FileDropLocation, FileID.ToString()+ ".docx"));
+            document.SaveAs2(FileName: Path.Combine(Properties.Settings.Default.FileDropLocation, fileID.ToString() + ".docx"));
             document.Close();
             application.Quit();
 
-            var File = new File()
+            var file = new File()
             {
-                ID = FileID.ToString(),
-                FileUrl = Path.Combine(Properties.Settings.Default.FileDropLocation, FileID.ToString() + ".docx").ToString(),
-                VisitorID = ID
+                ID = fileID.ToString(),
+                FileUrl = Path.Combine(Properties.Settings.Default.FileDropLocation, fileID.ToString() + ".docx").ToString(),
+                VisitorID = id
             };
             using (var db = new VisitorRegistrationEntities())
             {
-                db.Files.Add(File);
+                db.Files.Add(file);
                 db.SaveChanges();
             }
 
-            return FileID;
+            return fileID;
         }
 
-        public byte[] DownloadVisitorPass(Guid FileID)
+        /// <summary>
+        /// Starts the Download of the Visitor Pass
+        /// </summary>
+        /// <param name="fileID">File ID</param>
+        /// <returns>Returns the file as <see cref="byte"/> Array</returns>
+        public byte[] DownloadVisitorPass(Guid fileID)
         {
-            var FileEntry = new Wcf.File();
-            byte[] FileBinary = null;
+            var fileEntry = new Wcf.File();
+            byte[] fileBinary = null;
             using (var db = new VisitorRegistrationEntities())
             {
-                var F = from v in db.Files where v.ID == FileID.ToString() select v;
+                var f = from v in db.Files where v.ID == fileID.ToString() select v;
 
-                FileEntry = F.FirstOrDefault();
-
-
-                
-                FileBinary = System.IO.File.ReadAllBytes(Path.Combine(Properties.Settings.Default.FileDropLocation, FileEntry.FileUrl));
+                fileEntry = f.FirstOrDefault();
+                fileBinary = System.IO.File.ReadAllBytes(Path.Combine(Properties.Settings.Default.FileDropLocation, fileEntry.FileUrl));
             }
 
             using (var db = new VisitorRegistrationEntities())
             {
-                int VisitorID = 0;
-                int.TryParse(FileEntry.VisitorID.ToString(), out VisitorID);
+                int.TryParse(fileEntry.VisitorID.ToString(), out int visitorID);
                 var visitor = (from v in db.Visitors
-                            where v.ID == VisitorID  
+                            where v.ID == visitorID  
                             select v).FirstOrDefault();
 
                 visitor.Printed  = true;
@@ -152,7 +183,7 @@ namespace VisitorRegistration.Wcf
                 db.SaveChanges();
             }
 
-            return FileBinary;
+            return fileBinary;
         }
     }
 }
--- VisitorRegistration.Wcf/IEndpoint.cs
+++ VisitorRegistration.Wcf/IEndpoint.cs
@@ -1,24 +1,67 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.ServiceModel;
-using System.Text;
-
+﻿//-----------------------------------------------------------------------
+// <copyright file="IEndpoint.cs" company="None">
+// Copyright 2017 Jhueppauff
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// </copyright>
+//-----------------------------------------------------------------------
 namespace VisitorRegistration.Wcf
 {
+    using System;
+    using System.Collections.Generic;
+    using System.ServiceModel;
+
+    /// <summary>
+    /// IEndpoint for the WCF <see cref="Endpoint"/> Class
+    /// </summary>
     [ServiceContract]
     public interface IEndpoint
     {
+        /// <summary>
+        /// Register a new Visitor
+        /// </summary>
+        /// <param name="firstName">First Name of the Visitor</param>
+        /// <param name="lastName">Last Name of the Visitor</param>
+        /// <param name="email">eMail of the Visitor</param>
+        /// <param name="hostEmail">eMail of the responsible Host</param>
+        /// <param name="idNumber">ID Number</param>
+        /// <param name="signature">Signature Bash64 String</param>
         [OperationContract]
-        void RegisterVisitor(string FirstName, string LastName, string Email, string HostEmail, string IDNumber, string signature);
+        void RegisterVisitor(string firstName, string lastName, string email, string hostEmail, string idNumber, string signature);
+
+        /// <summary>
+        /// Get Visitor by ID
+        /// </summary>
+        /// <param name="id">ID of the Visitor</param>
+        /// <returns><see cref="Visitor"/></returns>
         [OperationContract]
-        Visitor GetVisitor(int ID);
+        Visitor GetVisitor(int id);
+
+        /// <summary>
+        /// Get a List of the type<see cref="Visitor"/> from the daily visitors
+        /// </summary>
+        /// <returns>Returns a list of the daily <see cref="Visitor"/></returns>
         [OperationContract]
         List<Visitor> GetCurrentVisitors();
+
+        /// <summary>
+        /// Starts the Download of the Visitor Pass
+        /// </summary>
+        /// <param name="fileID">File ID</param>
+        /// <returns><see cref="Returns File as Byte"/></returns>
         [OperationContract]
-        byte[] DownloadVisitorPass(Guid ID);
+        byte[] DownloadVisitorPass(Guid fileID);
+
+        /// <summary>
+        /// Generates the Visitor Pass
+        /// </summary>
+        /// <param name="id">Visitor ID</param>
+        /// <returns>Returns the FileID as <see cref="Guid"/></returns>
         [OperationContract]
-        Guid GenerateVisitorPass(int ID);
+        Guid GenerateVisitorPass(int id);
     }
 }
